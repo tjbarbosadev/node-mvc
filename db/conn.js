987700135832e071500625f74803d1de328bbb76
Thiago Barbosa <tjbarbosa.dev@gmail.com>
2025-07-16 18:34:31 -0300

    fix: using .env for db data

--- db/conn.js
+++ db/conn.js
@@ -1,9 +1,16 @@
 import Sequelize from 'sequelize';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const sequelize = new Sequelize('nodemvc', 'root', 'admin123', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME, // Nome do banco
+  process.env.DB_USER, // Usuário
+  process.env.DB_PASSWORD, // Senha
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+  }
+);
 
 // Versão com async/await (recomendado)
 export const testConnection = async () => {
