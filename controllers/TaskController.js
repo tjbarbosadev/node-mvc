e59ca858fc8b8720b7f7ae71e7ca7b37d887e6ea
Thiago Barbosa <tjbarbosa.dev@gmail.com>
2025-07-17 19:00:36 -0300

    feat: deleting items

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -1,4 +1,3 @@
-import { raw } from 'express';
 import Task from '../models/Task.js';
 
 class TaskController {
@@ -6,24 +5,55 @@ class TaskController {
     res.render('tasks/create');
   }
 
+  // CREATE
   static async createTaskSave(req, res) {
     const { title, description } = req.body;
     const task = { title, description, done: false };
     await Task.create(task);
-    res.redirect('tasks');
+    res.redirect('/tasks');
   }
 
+  // READ
   static async showTasks(req, res) {
     const tasks = await Task.findAll({ raw: true });
-    console.log(tasks);
     res.render('tasks/all', { tasks });
   }
 
   static async showTask(req, res) {
     const { id } = req.params;
     const task = await Task.findOne({ where: { id }, raw: true });
-    console.log(task);
     res.render('tasks/task', task);
+  }
+
+  static async editTask(req, res) {
+    const { id } = req.params;
+    const task = await Task.findOne({ where: { id }, raw: true });
+    res.render('tasks/edit', task);
+  }
+
+  // UPDATE
+  static async editTaskPost(req, res) {
+    const { id, title, description } = req.body;
+    await Task.update({ title, description }, { where: { id } });
+    res.redirect('/tasks');
+  }
+
+  static async updateTaskStatus(req, res) {
+    const { id } = req.body;
+    const task = {
+      done: req.body.done === '0' ? true : false,
+    };
+    await Task.update(task, { where: { id } });
+    res.redirect('/tasks');
+  }
+
+  // DELETE
+  static async deleteTask(req, res) {
+    const { id } = req.body;
+    console.log(id);
+
+    await Task.destroy({ where: { id } });
+    res.redirect('/tasks');
   }
 }
 
--- routes/TaskRoutes.js
+++ routes/TaskRoutes.js
@@ -7,6 +7,12 @@ taskRouter.get('/add', TaskController.createTask);
 taskRouter.post('/add', TaskController.createTaskSave);
 
 taskRouter.get('/:id', TaskController.showTask);
+taskRouter.get('/edit/:id', TaskController.editTask);
+
+taskRouter.post('/edit', TaskController.editTaskPost);
+taskRouter.post('/updatestatus', TaskController.updateTaskStatus);
+
+taskRouter.post('/remove', TaskController.deleteTask);
 
 taskRouter.get('/', TaskController.showTasks);
 
